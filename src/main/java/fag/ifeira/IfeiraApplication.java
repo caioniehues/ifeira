efc730ebf6e3844477bb05af928c1b6784ef1440
caioniehues <66445709+caioniehues@users.noreply.github.com>
2021-09-28 16:25:21 -0300

    Add files via upload

---
+++ src/main/java/fag/ifeira/IfeiraApplication.java
@@ -0,0 +1,13 @@
+package fag.ifeira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfeiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfeiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fag/ifeira/IfeiraApplicationTests.java
@@ -0,0 +1,13 @@
+package fag.ifeira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfeiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
