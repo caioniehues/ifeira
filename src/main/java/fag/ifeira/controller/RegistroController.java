6f0935d5e9f36ae7caafae837a17fac465da3201
caioniehues <cniehues1@gmail.com>
2021-09-28 22:08:44 -0300

    Criação das entidades básicas

--- src/main/java/fag/ifeira/controller/RegistroController.java
+++ src/main/java/fag/ifeira/controller/RegistroController.java
@@ -1,6 +1,17 @@
 package fag.ifeira.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/registro")
 public class RegistroController
 {
+
+    @GetMapping()
+    public String fodase(){
+        return "chupa meu cu";
+    }
 
 }
