52def5f1ca55a7c7a1ed9411644879f6601c1e96
caioniehues <cniehues1@gmail.com>
2021-10-01 21:13:32 -0300

    API FEITA CARALHO

--- src/main/java/fag/ifeira/controller/RegistroController.java
+++ src/main/java/fag/ifeira/controller/RegistroController.java
@@ -3,45 +3,25 @@ package fag.ifeira.controller;
 import fag.ifeira.entities.Usuario;
 import fag.ifeira.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/registro")
+@RequestMapping("")
 public class RegistroController
 {
 
     @Autowired
     UsuarioRepository usuarioRepository;
 
-    @GetMapping()
-    public String fodase(){
-        return "teste de requisição get";
-    }
 
-    @GetMapping("/teste")
-    public List<Usuario> teste(){
-
+    @PostMapping("/registrar")
+    public List<Usuario> registraUsuario(@RequestBody Usuario usuario){
+        usuarioRepository.save(new Usuario(usuario.getEmail(), usuario.getSenha(), usuario.getNome()));
         return usuarioRepository.findAll();
     }
 
-    @PostMapping("/user")
-    public String testeDeUsuario(){
-        Usuario usuario = new Usuario("caioniehues@gmail.com", "testedesenha");
-        Usuario usuario1 = new Usuario("caioniehues@gmail.com", "testedesenha");
-        Usuario usuario2 = new Usuario("caioniehues@gmail.com", "testedesenha");
-        Usuario usuario3 = new Usuario("caioniehues@gmail.com", "testedesenha");
-        Usuario usuario4 = new Usuario("caioniehues@gmail.com", "testedesenha");
-        usuarioRepository.save(usuario);
-        usuarioRepository.save(usuario1);
-        usuarioRepository.save(usuario2);
-        usuarioRepository.save(usuario3);
-        usuarioRepository.save(usuario4);
-        return "teste de salvar";
-    }
+
 
 }
--- src/main/java/fag/ifeira/entities/Usuario.java
+++ src/main/java/fag/ifeira/entities/Usuario.java
@@ -5,32 +5,33 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.List;
 
 @Table(name = "usuario")
 @Entity
 @Getter
 @Setter
-public class Usuario {
+public class Usuario implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ID_USUARIO")
     @SequenceGenerator(name = "ID_USUARIO", sequenceName = "ID_USUARIO", allocationSize = 1)
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "ativo", nullable = false, unique = true)
+    @Column(name = "ativo", nullable = false)
     private Boolean ativo = false;
 
-    @Column(name = "email", nullable = false, unique = true)
+    @Column(name = "email", nullable = false)
     private String email;
 
-    @Column(name = "senha", nullable = false, unique = true)
+    @Column(name = "senha", nullable = false)
     private String senha;
 
-    @Column(name = "telefone", unique = true)
+    @Column(name = "telefone")
     private String telefone;
 
-    @Column(name = "nome")
+    @Column(name = "nome", nullable = false)
     private String nome;
 
     @Column(name = "cpf", unique = true)
@@ -40,18 +41,19 @@ public class Usuario {
     private String cnpj;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "tipo_de_usuario", nullable = false, unique = true)
+    @Column(name = "tipo_de_usuario", nullable = false)
     private TipoDeUsuario tipoDeUsuario;
 
-    /*@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Pedido> pedidos;
 
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Endereco> enderecoes;*/
+    private List<Endereco> enderecoes;
 
-    public Usuario(String email, String senha) {
+    public Usuario(String email, String senha, String nome) {
         this.email = email;
         this.senha = senha;
+        this.nome = nome;
         this.ativo = true;
         this.tipoDeUsuario = TipoDeUsuario.CLIENTE;
     }
