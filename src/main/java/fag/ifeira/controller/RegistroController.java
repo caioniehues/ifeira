8c18427f6f6e9863062624d4635f6239e531954f
caioniehues <cniehues1@gmail.com>
2021-10-06 19:13:38 -0300

    sei lá na moral

--- src/main/java/fag/ifeira/controller/RegistroController.java
+++ src/main/java/fag/ifeira/controller/RegistroController.java
@@ -3,6 +3,7 @@ package fag.ifeira.controller;
 import fag.ifeira.entities.Usuario;
 import fag.ifeira.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,8 +17,8 @@ public class RegistroController
     UsuarioRepository usuarioRepository;
 
 
-    @PostMapping("/registrar")
-    public List<Usuario> registraUsuario(@RequestBody Usuario usuario){
+    @PostMapping(value = "/registrar", consumes = MediaType.APPLICATION_JSON_VALUE, produces = {MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public @ResponseBody List<Usuario> registraUsuario(@RequestBody Usuario usuario){
         usuarioRepository.save(new Usuario(usuario.getEmail(), usuario.getSenha(), usuario.getNome()));
         return usuarioRepository.findAll();
     }
